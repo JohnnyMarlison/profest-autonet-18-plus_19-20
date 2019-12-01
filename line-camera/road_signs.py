9f5f07e9a3928707aa8a2952cdd7c472428161df
user <rena040500@gmail.com>
2019-12-01 20:59:54 +0300

    Fix road_signs hsv.

--- line-camera/road_signs.py
+++ line-camera/road_signs.py
@@ -1,12 +1,11 @@
 import cv2
-import numpy as np
 
 def nothing(x):
     pass
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(-1)
 
-cv2.namedWindow('Result')
+cv2.namedWindow('result')
 
 cv2.createTrackbar('min_blue', 'result', 0, 255, nothing)
 cv2.createTrackbar('min_green', 'result', 0, 255, nothing)
@@ -16,43 +15,36 @@ cv2.createTrackbar('max_blue', 'result', 0, 255, nothing)
 cv2.createTrackbar('max_green', 'result', 0, 255, nothing)
 cv2.createTrackbar('max_red', 'result', 0, 255, nothing)
 
-#color = cv2.imread('color.jpg')
+color = cv2.imread('color.jpg')
 
 while (True):
     ret, frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     cv2.imshow("hsv", hsv)
 
-    min_blue = cv2.createTrackbar('min_blue', 'result', 0, 255, nothing)
-    min_green = cv2.createTrackbar('min_green', 'result', 0, 255, nothing)
-    min_red = cv2.createTrackbar('min_red', 'result', 0, 255, nothing)
+    min_blue = cv2.getTrackbarPos('min_blue', 'result')
+    min_green = cv2.getTrackbarPos('min_green', 'result')
+    min_red = cv2.getTrackbarPos('min_red', 'result')
 
-    max_blue = cv2.createTrackbar('max_blue', 'result', 0, 255, nothing)
-    max_green = cv2.createTrackbar('max_green', 'result', 0, 255, nothing)
-    max_red = cv2.createTrackbar('max_red', 'result', 0, 255, nothing)
+    max_blue = cv2.getTrackbarPos('max_blue', 'result')
+    max_green = cv2.getTrackbarPos('max_green', 'result')
+    max_red = cv2.getTrackbarPos('max_red', 'result')
 
+    hsv = cv2.blur(hsv, (5, 5))
 
     mask = cv2.inRange(hsv, (min_blue, min_green, min_red), (max_blue, max_green, max_red))
-    cv2.imshow('mask1', mask)
+    cv2.imshow('mask', mask)
 
-    result = cv2.bitwise_and(hsv, hsv, mask = mask)
-    cv2.imshow('Result1', result)
+    result = cv2.bitwise_and(frame, frame, mask = mask)
+    cv2.imshow('result', result)
 
-    #mask2 = cv2.inRange(color, (min_blue, min_green, min_red), (max_blue, max_green, max_red))
-    #cv2.imshow('Mask2', frame)
 
-    #result2 = cv2.bitwise_and(color, frame, mask=mask)
-    #cv2.imshow('Result2', frame)
-
-    # Display the resulting frame
-    #cv2.imshow("Stream", frame)
 
     # Press Q on keyboard to stop recording
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    # if cv2.waitKey(1) & 0xFF == ord('q'):
+    #     break
+    if cv2.waitKey(1) == 27:
         break
 
-# When everything done, release the video capture and video write objects
 cap.release()
-
-# Closes all the frames
 cv2.destroyAllWindows()

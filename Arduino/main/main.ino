c642314416156c0d10a393d71864603f87b6ae59
L1PK4 <ostrishko.danila@gmail.com>
2020-01-20 15:51:26 +0300

    Last ver

--- Arduino/main/main.ino
+++ Arduino/main/main.ino
@@ -3,7 +3,7 @@
 #define DirectionPin    (10)
 #define BaudRate        (57600)
 #define MAX_POSITION    (30)
-#define BASIC_SPEED     (200)
+#define BASIC_SPEED     (150)
 #define COMMAND_LEN     (2)
 #define LEFT_MOTOR_A    (A0)
 #define LEFT_MOTOR_B    (A1)
@@ -15,23 +15,12 @@
 #define TIMEOUT         (100)
 #define ZERO            (480)
 
-
-
-class Dynamixel : public AX12A
-{
-    private:
-        int pos;
-
-    public:
-    Dynamixel()
-    {
-        begin(BaudRate, DirectionPin, &Serial2);
-        setEndless(ID, OFF);
-        pos = 0;
-    }
+int pos;
+int turn_angle = 0;
 
     void setPos(int _pos = 0)
     {
+        _pos *= -1;
         if (_pos  > MAX_POSITION)
         {
             pos = static_cast <int> (ZERO + 3.41 * MAX_POSITION);
@@ -48,37 +37,42 @@ class Dynamixel : public AX12A
 
     int getPos()
     {
-        return readPosition(ID);
+        return ax12a.readPosition(ID);
     }
 
     void turn()
     {
-        int time = millis();
+        int timestamp = millis();
         while (pos != getPos())
         {
-            moveSpeed(ID,pos,BASIC_SPEED);
-            delay(100);
-            if(millis() - time >= TIMEOUT) return;
+            ax12a.moveSpeed(ID, pos, BASIC_SPEED);
+            delay(25);
+            if(millis() - timestamp >= TIMEOUT) return;
         }
     }
     void set_and_turn(int _pos = 0)
     {
-      setPos(_pos);
+      turn_angle += _pos;
+      if (turn_angle > 20) turn_angle = 20;
+      if (turn_angle < -20) turn_angle = -20;
+      setPos(turn_angle);
       turn();
     }
 
-};
-
-uint8_t com[COMMAND_LEN];
-Dynamixel srv;
+char com = 'S';
 
 void setup() 
 {
-    Serial.begin(9600);
-    srv.set_and_turn(0);
+  
+    ax12a.begin(BaudRate, DirectionPin, &Serial2);
+    ax12a.setEndless(ID, OFF);
+    pos = 0;
+    Serial3.begin(9600);
+    set_and_turn(0);
+    pinMode(13,OUTPUT);
 }
 
-void drive(int speed)
+void drive(int speed = 190)
 {
     if(abs(speed) > 255) return;
     if (speed == 0) 
@@ -99,13 +93,54 @@ void stop()
     analogWrite(RIGHT_MOTOR_B,0);
 }
 
+int time_recive = 1000, time_notrecive;
 
 void loop()
 {
-    if (Serial.available())
+    if (Serial3.available() > 0)
     {
-        Serial.readBytes(com, COMMAND_LEN);
-        drive(2 * (com[0] - 128));
-        srv.set_and_turn(com[1] - 30);
+      time_recive = millis();
+      com = Serial3.read();
+        switch(com)
+        {
+          case 'S':
+            stop();
+            break;
+          case 'F':
+            drive();
+            break;
+          case 'L':
+            set_and_turn(2);
+            break;
+          case 'R':
+            set_and_turn(-2);
+            break;
+          case 'B':
+            drive(-190);
+            break;
+          case 'W':
+            digitalWrite(13,HIGH);
+            break;
+          case 'w':
+            digitalWrite(13,LOW);
+            break; 
+          case 'I':
+            set_and_turn(-2);
+            drive();
+            break; 
+          case 'G':
+            set_and_turn(2);
+            drive();
+            break;
+            
+            
+            
+        }
+      }
+    else
+    {
+      time_notrecive = millis();
+      if(time_notrecive - time_recive > TIMEOUT)
+        stop();
     }
 }
